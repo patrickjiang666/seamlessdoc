8844180256dd89aacc17b5932432e73a9e0b2abf
patrick.J <vjiang666@gmail.com>
2018-03-06 19:44:21 -0500

    test frames console

--- assignment.js
+++ assignment.js
@@ -1,5 +1,9 @@
 const bowling = (str)=>{
 	let frames = str.replace(/-/g, '0').split(' ');
 	console.log(frames);
+	let score = 0;
+	frames.map((ele, index)=>{
+		console.log(ele,index);
+	});
 }
 bowling('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-');

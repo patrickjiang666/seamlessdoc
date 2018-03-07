cceb73eef93f51ee48c86e7ec5a9ffa121dc4c16
patrick.J <vjiang666@gmail.com>
2018-03-06 19:46:50 -0500

    test first score

--- assignment.js
+++ assignment.js
@@ -4,6 +4,11 @@ const bowling = (str)=>{
 	let score = 0;
 	frames.map((ele, index)=>{
 		console.log(ele,index);
+		if(ele.length == 2){
+			let first = ele.charAt(0);
+    	let second = ele.charAt(1);
+    	score += first + second;
+		}
 	});
 }
 bowling('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-');

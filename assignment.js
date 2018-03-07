44411943da4c20103c6e730a3bd1a3755748e9fd
patrick.J <vjiang666@gmail.com>
2018-03-06 20:55:03 -0500

    clean up

--- assignment.js
+++ assignment.js
@@ -42,61 +42,61 @@ const bowling = (str)=>{
   }
 /*---------------------------*/
 	frames.map((ele, index)=>{
-		if(ele=='X'){
-			if(queue.length==0){
-        queue.push('strike'); 
-      }
-      else if(queue.length==1){ // ... 81 5/ X 
-      	oneAhead();
-      }
-      else if(queue.length==2){
-        calc2Prev(ele);
-        oneAhead();
-      }
+	if(ele=='X'){
+		if(queue.length==0){
+			queue.push('strike'); 
 		}
-		else if(ele.length == 2){
-    	if(queue.length==0){
-    		twoAhead(ele);
-      }
-	    else if(queue.length==1){
-	    	calcPrev(ele);
-        twoAhead(ele);
-	    }
-	    else if(queue.length==2){ // ... 81 5/ X 
-	    	calc2Prev(ele);
-        calcPrev(ele);
-        twoAhead(ele);
-	    }
+		else if(queue.length==1){ // ... 81 5/ X 
+			oneAhead();
 		}
-		if(ele.length == 3 && index==frames.length-1){
-			let first = ele.charAt(0);
-      let second = ele.charAt(1);
-      let third = ele.charAt(2);
-      if(queue.length==0){
-      	if(second!='/' && third!=='/'){
-          score += (parseInt(second) + parseInt(third))*2 + 10;
-        }
-        else{
-          third=='X'?score += 30:score += (10+parseInt(third));
-        }
-      }
-      else if(queue.length==1){
-      	if(queue[0]=='spare'){
-          score += parseInt(first) + 20;
-          third=='X'?score += 10:score += parseInt(third);
-        }
-        else{
-          score += parseInt(first) + 30;
-          third=='X'?score += 20:score += (10+parseInt(third)*3);
-        }
-      }
-      else if(queue.length==2){
-        score += parseInt(first) + 40;
-        third=='X'?score += 20:score += (10+parseInt(third));
-      }
+		else if(queue.length==2){
+			calc2Prev(ele);
+			oneAhead();
 		}
-		console.log(score,queue);
-		return score;
+	}
+	else if(ele.length == 2){
+		if(queue.length==0){
+			twoAhead(ele);
+		}
+		else if(queue.length==1){
+			calcPrev(ele);
+			twoAhead(ele);
+		}
+		else if(queue.length==2){ // ... 81 5/ X 
+			calc2Prev(ele);
+			calcPrev(ele);
+			twoAhead(ele);
+		}
+	}
+	if(ele.length == 3 && index==frames.length-1){
+		let first = ele.charAt(0);
+		let second = ele.charAt(1);
+		let third = ele.charAt(2);
+		if(queue.length==0){
+			if(second!='/' && third!=='/'){
+				score += (parseInt(second) + parseInt(third))*2 + 10;
+			}
+			else{
+				third=='X'?score += 30:score += (10+parseInt(third));
+			}
+		}
+		else if(queue.length==1){
+			if(queue[0]=='spare'){
+				score += parseInt(first) + 20;
+				third=='X'?score += 10:score += parseInt(third);
+			}
+			else{
+				score += parseInt(first) + 30;
+				third=='X'?score += 20:score += (10+parseInt(third)*3);
+			}
+		}
+		else if(queue.length==2){
+			score += parseInt(first) + 40;
+			third=='X'?score += 20:score += (10+parseInt(third));
+		}
+	}
+	console.log(score,queue);
+	return score;
 	});
 }
 console.log(bowling('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-'));

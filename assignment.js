4011637257fe855ca7b46a1876f6a2332f0aa379
patrick.J <vjiang666@gmail.com>
2018-03-06 19:52:51 -0500

    use queue

--- assignment.js
+++ assignment.js
@@ -2,13 +2,20 @@ const bowling = (str)=>{
 	let frames = str.replace(/-/g, '0').split(' ');
 	console.log(frames);
 	let score = 0;
+	let queue = [];
 	frames.map((ele, index)=>{
 		console.log(ele,index);
 		if(ele.length == 2){
 			let first = ele.charAt(0);
     	let second = ele.charAt(1);
-    	score += first + second;
+    	if(second!='/'){
+				score += parseInt(first) + parseInt(second);
+			}
+			else{
+				queue.push('spare');
+			}
 		}
 	});
 }
 bowling('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-');
+bowling('5/ 5/ 26 X 81 5/ X 71 7/ 7/X');
